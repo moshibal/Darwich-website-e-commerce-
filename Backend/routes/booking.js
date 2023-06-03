e83cd0ef1c26e2e2f435c41025ac6f0b218a2c34
Bishal karki <bishalcarki2020@gmail.com>
2023-06-03 12:07:47 +1000

    fixing errors

--- Backend/routes/booking.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllBookings, postBooking } from "../controllers/booking.js";
-import { auth } from "../middleware/auth.js";
-const router = express.Router();
-router.get("/", auth, getAllBookings);
-router.post("/", postBooking);
-
-export default router;
